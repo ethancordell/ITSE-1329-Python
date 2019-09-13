341d775ea2f78f010ba12afaf9b9027cec674410
Ethan Crain <crainsquatch@gmail.com>
2019-09-13 14:20:01 +0000

    completed phase 1

---
+++ Chatbot/Chatbot-phase1-ethancrain.py
@@ -0,0 +1,6 @@
+#Ethan Crain
+first = input('What is your first name?: ')
+last = input('What is your last name?: ')
+initial = last[:1]
+time = input('What time of day is it?(Morning, Afternoon, Evening): ')
+print('Good ' + time + ' ' + first + ' ' + initial)
