e1247adc75567a30fea837d9d056aae7e43b2566
Ethan Crain <crainsquatch@gmail.com>
2019-09-25 13:18:01 +0000

    completed excercises

--- CC2/CC2-Ex14/code.py
+++ CC2/CC2-Ex14/code.py
@@ -1,3 +1,4 @@
 countdown = [5, 4, 3, 2, 1]
-
-# Your code goes here
+for i in countdown:
+    print(i)
+print('Blastoff!')
--- CC2/CC2-Ex15/code.py
+++ CC2/CC2-Ex15/code.py
@@ -1,3 +1,4 @@
 students = ['Andy', 'Clint', 'Rus', 'Tonya']
-
-# Your code goes here
+for student in students:
+    print('Welcome to class', student)
+print('Class has started')
